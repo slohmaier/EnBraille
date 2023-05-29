dd3f104fbe0a72b829f649a80269c0a377e0b645
Stefan Lohmaier <228552+slohmaier@users.noreply.github.com>
2023-05-29 09:25:49 +0200

    add table list to enbraille table widget

--- enbraille_widgets.py
+++ enbraille_widgets.py
@@ -7,6 +7,26 @@ class EnBrailleTableComboBox(QComboBox):
         super().__init__(parent)
 
         self._libbrl = libbrlImpl()
+        self._tables = self._libbrl.listTables()
         
-        for table in sorted(self._libbrl.listTables()):
-            self.addItem(table)        
+        for table in sorted(self._tables.keys()):
+            table_filename = self._tables[table]
+            self.addItem(table, table_filename)
+    
+    @property
+    def table(self) -> Optional[str]:
+        if self.currentIndex() >= 0:
+            return self.currentText()
+        else:
+            return None
+    
+    @table.setter
+    def table(self, value: str) -> None:
+        self.setCurrentText(value)
+    
+    @property
+    def tableFilename(self) -> Optional[str]:
+        if self.currentIndex() >= 0:
+            return self.itemData(self.currentIndex())
+        else:
+            return None
