b6533ced7cd2f2201ac5465140b33b0f33c0da51
Stefan Lohmaier <stefan@slohmaier.de>
2024-10-27 18:10:15 +0100

    change data in testcase

--- tests/test_reformat.py
+++ tests/test_reformat.py
@@ -10,5 +10,6 @@ class TestReformatBRF(unittest.TestCase):
     def test_reformat_simple(self):
         reformater = EnBrailleReformater(os.path.join(TESTFILE_DIR, 'reformat_simple.brf'))
         data = gen_data()
+        data.reformatLineLength = 0
         self.assertEqual('', reformater.reformat(None, data))
         
